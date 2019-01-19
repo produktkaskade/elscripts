3eda434413506fccda07b94846028cc1380a4158
produktkaskade <39365617+produktkaskade@users.noreply.github.com>
2019-01-19 10:33:03 +0100

    change check to indexOf insteaf of equals

--- EL_NavigateHotkeys.user.js
+++ EL_NavigateHotkeys.user.js
@@ -10,13 +10,22 @@
 
 (function() {
     'use strict';
+
+    var nextMessages = [">>", "nächste Nachricht"];
+    var prevMessages = ["<<", "letzte Nachricht"];
+
     var keyboardHandler = function (zEvent) {
+        var arr = Array.from(document.getElementsByTagName('A'));
         if (zEvent.code == "ArrowRight") {
-            var arr = Array.from(document.getElementsByTagName('A'));
-            var forwards = arr.filter(x=> x.innerText == ">>");
+            var forwards = arr.filter(x => nextMessages.some(t => x.innerText.indexOf(t) != -1));
             var forward = forwards[forwards.length-1];
             forward.click();
+        } else if (zEvent.code == "ArrowLeft") {
+            var prevs = arr.filter(x => prevMessages.some(t => x.innerText.indexOf(t) != -1));
+            var prev = prevs[prevs.length-1];
+            prev.click();
         }
+
     }
     window.addEventListener("keydown", keyboardHandler, false);
     // Your code here...
